8c883bca407048ba2df34ef1e70a20fda8b1c935
month <601011814@qq.com>
2019-03-31 01:23:59 +0800

    可运行初版

--- bin/mt-seed.js
+++ bin/mt-seed.js
@@ -1,22 +1,26 @@
 #! /usr/bin/env node
 
-const { resolve } = require('path');
-const res = command => resolve(__dirname, '../commands/', command);
-const program = require('commander');
+const { resolve } = require('path')
+const res = command => resolve(__dirname, '../commands/', command)
+const program = require('commander')
+// 定义版本号
+program.version(require('../package').version)
 
-program.version(require('../package').version);
-
-program.usage('<command> [options]');
+// 定义使用方法
+program.usage('<command>')
 
 program
-    .command('init')
-    // .option('-f, --foo', 'enable some foo')
-    .description('初始化项目')
-    .alias('i')
-    .action(() => {
-        require(res('init'))
-    });
+  .command('init')
+  .option('-f, --foo', 'enable some foo')
+  .description('初始化项目')
+  .alias('i')
+  .action(() => {
+    require(res('init'))
+  })
+
+// 必须执行以下语句转换命令行参数，否则不执行action函数
+program.parse(process.argv)
 
 if (!program.args.length) {
-    program.outputHelp();
+  program.outputHelp()
 }
--- commands/init.js
+++ commands/init.js
@@ -1,111 +1,111 @@
-const { prompt } = require('inquirer');
-const program = require('commander');
-const chalk = require('chalk');
-const download = require('download-git-repo');
-const ora = require('ora');
-const fs = require('fs');
+const { prompt } = require('inquirer')
+const program = require('commander')
+const chalk = require('chalk')
+const download = require('download-git-repo')
+const ora = require('ora')
+const fs = require('fs')
 
-const option = program.parse(process.argv).args[0];
-const defaultName = typeof option === 'string' ? option : '';
-const tpls = require(`../templates`);
-const tplKeys = Object.keys(tpls) || [];
+const option = program.parse(process.argv).args[0]
+const defaultName = typeof option === 'string' ? option : 'project-name'
+const tpls = require(`../templates`)
+const tplKeys = Object.keys(tpls) || []
 const question = [
-    {
-        type: 'input',
-        name: 'name',
-        message: '请输入项目名称',
-        default: defaultName,
-        filter (val) {
-            return val.trim()
-        },
-        validate (val) {
-            const validate = (val.trim().split(" ")).length === 1;
-            return validate || '项目名称不能包含空格';
-        },
-        transformer (val) {
-            return val;
-        }
+  {
+    type: 'input',
+    name: 'name',
+    message: '请输入项目名称',
+    default: defaultName,
+    filter (val) {
+      return val.trim()
     },
-    {
-        type: 'list',
-        name: 'template',
-        message: '请选择项目模板',
-        choices: tplKeys,
-        default: tplKeys[0],
-        validate (val) {
-            return true;
-        },
-        transformer (val) {
-            return val;
-        }
+    validate (val) {
+      const validate = (val.trim().split(' ')).length === 1
+      return validate || '项目名称不能包含空格'
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: '项目描述',
-        default: '',
-        validate (val) {
-            return true;
-        },
-        transformer (val) {
-            return val;
-        }
-    },
-    {
-        type: 'input',
-        name: 'author',
-        message: '项目作者',
-        default: 'author',
-        validate (val) {
-            return true;
-        },
-        transformer (val) {
-            return val;
-        }
+    transformer (val) {
+      return val
     }
-];
+  },
+  {
+    type: 'list',
+    name: 'template',
+    message: '请选择项目模板',
+    choices: tplKeys,
+    default: tplKeys[0],
+    validate (val) {
+      return true
+    },
+    transformer (val) {
+      return val
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '项目描述',
+    default: '',
+    validate (val) {
+      return true
+    },
+    transformer (val) {
+      return val
+    }
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '项目作者',
+    default: 'author',
+    validate (val) {
+      return true
+    },
+    transformer (val) {
+      return val
+    }
+  }
+]
 
-module.exports = prompt(question).then(({name, template, description, author}) => {
-    const projectName = name;
-    const gitPlace = tpls[template]['place'];
-    const gitBranch = tpls[template]['branch'];
-    const spinner = ora('正在初始化项目...');
-    spinner.start();
+module.exports = prompt(question).then(({ name, template, description, author }) => {
+  const projectName = name
+  const gitPlace = tpls[template]['place']
+  const gitBranch = tpls[template]['branch']
+  const spinner = ora('正在初始化项目...')
+  spinner.start()
 
-    download(`${gitPlace}${gitBranch}`, `./${projectName}`, (err) => {
+  download(`${gitPlace}${gitBranch}`, `./${projectName}`, (err) => {
+    if (err) {
+      console.log(chalk.red(err))
+      process.exit()
+    }
+
+    fs.readFile(`./${projectName}/package.json`, 'utf8', function (err, data) {
+      if (err) {
+        spinner.stop()
+        console.error(err)
+        return
+      }
+      const packageJson = JSON.parse(data)
+      packageJson.name = name
+      packageJson.description = description
+      packageJson.author = author
+      const updatePackageJson = JSON.stringify(packageJson, null, 2)
+
+      fs.writeFile(`./${projectName}/package.json`, updatePackageJson, 'utf8', function (err) {
         if (err) {
-            console.log(chalk.red(err));
-            process.exit()
-        }
-
-        fs.readFile(`./${projectName}/package.json`, 'utf8', function (err, data) {
-            if (err) {
-                spinner.stop();
-                console.error(err);
-                return;
-            }
-            const packageJson = JSON.parse(data);
-            packageJson.name = name;
-            packageJson.description = description;
-            packageJson.author = author;
-            const updatePackageJson = JSON.stringify(packageJson, null, 2);
-
-            fs.writeFile(`./${projectName}/package.json`, updatePackageJson, 'utf8', function (err) {
-                if (err) {
-                    spinner.stop();
-                    console.error(err);
-                    return;
-                } else {
-                    spinner.stop();
-                    console.log(chalk.green('项目初始化成功'));
-                    console.log(`
+          spinner.stop()
+          console.error(err)
+          return
+        } else {
+          spinner.stop()
+          console.log(chalk.green('项目初始化成功'))
+          console.log(`
                         ${chalk.bgWhite.black('   Run Application  ')}
                         ${chalk.yellow(`cd ${name}`)}
                         ${chalk.yellow('npm install')}
                         ${chalk.yellow('npm start')}
-                    `);
-                }
-            });
-        });
-    });
-});
+                    `)
+        }
+      })
+    })
+  })
+})
